dfa54e5dd031982c54e3b6ed90734c48143451d4
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-19 15:08:58 +0900

    21 トップページのレイアウトを更新する

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
+/* eslint-disable @next/next/link-passhref */
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
     <Layout>
-      <a href=''>Nextjsです</a>
+      <section className={utilStyles.headingMd}>
+        <p>私はプログラミング学習中の者です/好きな言語はPHP・Ruby・JavaScriptです</p>
+      </section>
+
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" />
+            </Link>
+            <Link href="/">
+              <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+            </Link>
+            <br />
+            <small>
+              February 23, 2020
+            </small>
+          </article>
+        </div>
+      </section>
     </Layout>
   )
 }
