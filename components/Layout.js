8d17d77e80664cad215695c4570a8091d9ccf0e7
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-21 16:31:35 +0900

    40 ブログの細かい訂正(その1)

--- components/Layout.js
+++ components/Layout.js
@@ -8,15 +8,24 @@ export const siteTitle = 'Next.js blog'
 
 const name = 'Taka Code'
 
-function Layout({ children }) {
+function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" heref="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" className={utilStyles.borderCircle} />
-        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        {home ? (
+          <>
+            <img src="/images/profile.png" className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`} />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img src="/images/profile.png" className={`${utilStyles.borderCircle}`} />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
       </header>
       <main>{children}</main>
     </div>
